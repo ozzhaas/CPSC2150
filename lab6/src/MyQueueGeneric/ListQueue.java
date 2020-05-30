c062e5cc115d9ee391f8f10dff689a617a2abc71
sterlir <56045200+sterlir@users.noreply.github.com>
2020-05-30 19:45:32 -0400

    Add files via upload

--- lab6/src/MyQueueGeneric/ListQueue.java
+++ lab6/src/MyQueueGeneric/ListQueue.java
@@ -1,4 +1,59 @@
-package MyQueueGeneric;
+/* Sterling Rich and Kellen Haas
+  CPSC 2150
+  Lab5
+  5/26/20
+ */
 
-public class ListQueue {
+package cpsc2150.MyQueue;
+
+import java.util.*;
+
+public class ListQueue<T> extends AbsQueue<T> {
+
+    /**
+     * @invariants 0 <= depth <= MAX_DEPTH and
+     * [depth = number of Ts in myQ]
+     * Correspondence IQueue = myQ
+     */
+
+    //this time store the queue in a list
+    //myQ.get(0) is the front of the queue
+    private List<T> myQ;
+
+    /**
+     * @post myQ.size() = 0 and [myQ is empty]
+     */
+    public ListQueue() {
+        myQ = new ArrayList<T>();
+    }
+
+    /**
+     * @pre myQ.size() < MAX_DEPTH
+     * @post myQ.size() = #myQ.size() + 1 and x = myQ[myQ.size()]
+     * @param x The T being pushed into the end of the Queue
+     */
+    public void add(T x){
+        myQ.add(x);
+    }
+
+    /**
+     * @pre myQ.size() > 0
+     * @post myQ.size() = #myQ.size() - 1
+     * @return Returns the first T in the Queue
+     */
+    public T pop() {
+        T x = myQ.get(0);
+
+        myQ.remove(0);
+        return x;
+    }
+
+    /**
+     * @post 0 <= myQ.size() <= MAX_DEPTH
+     * @return The current amount of Ts in myQ
+     */
+    public int size() {
+        return myQ.size();
+    }
 }
+
