28b03672018ffeaa8866c5223346fe2e36e1fb21
ozzhaas <kellenhaas@outlook.com>
2020-05-31 11:53:51 -0400

    5.31 11:52am

---
+++ lab6/src/MyQueue/AbsQueue.java
@@ -0,0 +1,25 @@
+/* Sterling Rich and Kellen Haas
+  CPSC 2150
+  Lab5
+  5/26/20
+ */
+
+package cpsc2150.MyQueue;
+
+import java.lang.*;
+import java.util.*;
+
+public abstract class AbsQueue<T> implements cpsc2150.MyQueue.IQueue<T> {
+
+    @Override
+    public String toString(){
+
+        String qString = "";
+
+        for(int i = 1; i < size() + 1;i++){
+            qString += get(i).toString();
+        }
+
+        return qString;
+    }
+}
---
+++ lab6/src/MyQueue/ArrayQueue.java
@@ -0,0 +1,67 @@
+/* Sterling Rich and Kellen Haas
+  CPSC 2150
+  Lab5
+  5/26/20
+ */
+
+package cpsc2150.MyQueue;
+
+public class ArrayQueue<T> extends AbsQueue<T> {
+
+    /**
+     * @invariants 0 <= depth <= MAX_DEPTH and
+     *          [depth = number of Ts in myQ]
+     * Correspondence IQueue = myQ
+     */
+
+    // where the data is stored. myQ[0] is the front of the queue
+    private T [] myQ;
+
+    //tracks how many items in the queue
+    // also used to find the end of the queue
+    private int depth;
+
+    /**
+     * @post myQ size = MAX_DEPTH and depth = 0
+     */
+    public ArrayQueue() {
+        myQ = (T[]) new Object [MAX_DEPTH];
+        depth = 0;
+    }
+
+    /**
+     * @pre depth < MAX_DEPTH
+     * @post depth = #depth + 1 and x = myQ[depth]
+     * @param x The T being pushed into the end of the Queue
+     */
+    public void add(T x){
+
+        myQ[depth] = x;
+        depth++;
+    }
+
+    /**
+     * @pre depth > 0
+     * @post depth = #depth - 1
+     * @return Returns the first T in the Queue
+     */
+    public T pop() {
+        T x = myQ[0];
+
+        for(int i = 0; i < depth;i++){
+            myQ[i] = myQ[i+1];
+        }
+
+        depth--;
+        return x;
+    }
+
+    /**
+     * @post 0 <= depth <= MAX_DEPTH
+     * @return The current amount of Ts in myQ
+     */
+    public int size() {
+        return depth;
+    }
+
+}
